119bac62c42a50a494f63c79c4cf6fc0ae32fc66
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-03-09 15:41:07 -0300

    Connecting to h2 in memory database and creating the table for users

--- server/src/main/java/com/vsanches/tddrestapi/config/SecurityConfig.java
+++ server/src/main/java/com/vsanches/tddrestapi/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -29,7 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().antMatchers("/h2-console").permitAll();
+	    http.headers().frameOptions().sameOrigin();
 	}
 
 	@Bean
