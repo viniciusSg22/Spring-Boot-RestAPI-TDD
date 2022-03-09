65fb81a1521e9e5a47947bc9365777611a156796
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2022-03-09 14:10:57 -0300

    Importing maven project from spring initializr

---
+++ server/src/main/java/com/vsanches/tddrestapi/TddRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.vsanches.tddrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddRestapiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/vsanches/tddrestapi/TddRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsanches.tddrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
